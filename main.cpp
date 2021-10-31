ac79b07c7d4e0c00e302959b81e77dd9ad2c63e4
kaixindeken <zra981217@163.com>
2021-10-31 09:20:09 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+//
+// main.cpp
+// cpp_docker
+//
+// created by changkun at shiyanlou.com
+// 
+
+#include "docker.hpp"
+#include <iostream>
+
+int main(int argc, char** argv) {
+    std::cout << "...start container" << std::endl;
+    docker::container_config config;
+    config.host_name = "shiyanlou";
+    config.root_dir  = "./shiyanlou";
+    config.ip        = "192.168.0.100";
+    config.bridge_name = "docker0";
+    config.bridge_ip   = "192.168.0.1";
+    docker::container container(config);
+    container.start();
+    std::cout << "stop container..." << std::endl;
+    return 0;
+}
